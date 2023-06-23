cbfe7455d554b4b1c952ce0e38b37866fa30f532
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-23 19:59:17 +0530

    enable cors and code cleanup

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const productRoute = require('./routes/products');
 
 const app = express();
-const morgan = require('morgan');
-require('dotenv').config(); 
-const connectDB = require('./mongoConnect');
 
+require('dotenv').config(); 
+
+const connectDB = require('./mongoConnect');
 connectDB();
 
 const api = process.env.API_URL;
 
-const PORT = 3000 ;
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 ////////////////       Middleware Section             //////////////////
+
+// Enable CORS
+app.use(cors());
+app.options('*', cors());
 
 // Express Middleware to parse/handle incoming and outgoing requests
 app.use(express.json());
@@ -28,21 +33,6 @@ app.use(`${api}/products`, productRoute);
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id : 1,
-        name: 'Eshwa',
-        image: 'some dummy image'
-    }
-    res.send(product)
-})
-
-app.post(`${api}/products`, (req, res) => {
-    const product = req.body;
-    console.log(product);
-    res.send(product)
-})
 
 app.listen(PORT, () => {
     console.log(`The port is up on port: ${PORT}`);
