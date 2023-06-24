d4db8726584b813fa9ccbb65bf51d3481152487a
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-24 10:21:40 +0530

    schema for orders and categories

--- models/category.js
+++ models/category.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-
+    name: { type: String, required: true },
+    icon: { type: String },
+    color: { type: String }
 });
 
 module.exports = mongoose.model('Category', categorySchema);
--- models/products.js
+++ models/products.js
@@ -4,8 +4,18 @@ const { Schema } = mongoose;
 
 const productSchema = new Schema({
     name: { type: String, required: true },
-    image: { type: String, required: true },
-    countInStock: { type: Number, require: true }
+    description: { type: String, required: true },
+    richDescription: { type: String, default: '' }, 
+    image: { type: String, default: '' }, 
+    images: [{ type: String }],
+    brand: { type: String, default: '' },
+    price: { type: Number, default: 0 },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true, min: 0, max: 255 },
+    countInStock: { type: Number, require: true },
+    rating: { type: Number, default: 0 },
+    numReviews: { type: Number, default: 0 },
+    isFeatuerd: { type: Boolean, default: false },
+    dateCreated: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('Product', productSchema);
