1eb386f0a36fbdf98c9aa132c155d2449b0b73c7
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-05 17:44:24 +0530

    create: user schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,22 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = Schema({
-
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    passwordHash: { type: String, required: true },
+    phone: { type: String, required: true },
+    isAdmin: { type: Boolean, required: false },
+    street: { type: String, default: '' },
+    apartment: { type: String, default: '' },
+    city: { type: String, default: '' },
+    zip: { type: String, default: '' },
+    country: { type: String, default: '' }
 });
+
+productSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+})
+
+productSchema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model('User', userSchema);
