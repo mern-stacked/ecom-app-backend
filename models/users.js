2160f176c81ee8b9d72e9580f468326ab56724a0
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-11 23:02:51 +0530

    processing the password before processing

--- models/users.js
+++ models/users.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const { Schema } = mongoose;
 
 const userSchema = Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    passwordHash: { type: String, required: true },
+    password: { type: String, required: true, minlength: 6 },
     phone: { type: String, required: true },
     isAdmin: { type: Boolean, required: false },
     street: { type: String, default: '' },
@@ -20,5 +21,46 @@ userSchema.virtual('id').get(function () {
 })
 
 userSchema.set('toJSON', { virtuals: true });
+
+// Preprocessing the password
+userSchema.pre('save', function(next) {
+
+    const user = this;
+
+    if(!user.isModified('password')){
+        return(next);
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+
+        if(err) { return next(err); }
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+
+            if(err) { return next(err); }
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// Compare the password generated during signup with the one while login 
+userSchema.methods.comparePassword = function(candidatePassword) {
+
+    const user = this;
+
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+
+            if(err) return reject(err);
+
+            if(!isMatch) return reject(false)
+
+            resolve(true);
+        })
+    });
+}
+
 
 module.exports = mongoose.model('User', userSchema);
