28a955fe70e5ffb91c87c572ebaffccef885a824
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-12 18:00:40 +0530

    exclude rest api routes from authentication

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -6,6 +6,13 @@ function authJwt() {
     return jwt({
         secret,
         algorithms: ['HS256']
+    }).unless({
+        path : [
+            { url: /\/api\/v1\/products(.*)/ , methods: [ 'GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/ , methods: [ 'GET', 'OPTIONS'] },
+            '/api/v1/users/register',
+            '/api/v1/users/login'
+        ]
     })
 }
 
