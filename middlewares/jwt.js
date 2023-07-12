f89b208b45a9897afe0baa7ff01faa0a10d7bfce
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-12 23:43:57 +0530

    bug fix

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -6,20 +6,21 @@ function authJwt() {
     return jwt({
         secret,
         algorithms: ['HS256'],
-        isRevoked: isRevoked
+        isRevoked: isRevoked,
     }).unless({
         path : [
             { url: /\/api\/v1\/products(.*)/ , methods: [ 'GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/ , methods: [ 'GET', 'OPTIONS'] },
             '/api/v1/users/register',
-            '/api/v1/users/login'
+            '/api/v1/users/login',
         ]
     })
 }
 
 async function isRevoked(req, payload) {
+    
     // if our user makes a authorized api call and is not admin then reject the token
-    if(payload.isAdmin == false){
+    if(payload.payload.isAdmin == false){
         // not a admin : reject the token
         return true;
     } 
