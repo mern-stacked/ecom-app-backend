ec375586a48ecfff35ca8eb9a029a26fda012d97
eshwagadkar <eshwagadkar007@gmail.com>
2023-08-15 19:38:52 +0530

    create order schema

---
+++ models/order-item.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderItemSchema = new Schema({
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+})
+
+orderItemSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+})
+
+orderItemSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('OrderItem', orderItemSchema);
